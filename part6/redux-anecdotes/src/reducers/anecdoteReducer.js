578874eca79d203244d330a25a02c691f9e6d3c5
emmettxt <emmet.txt@gmail.com>
2022-09-15 12:41:15 +0200

    Refactored anecdoteReducer to use Redux toolkit

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,48 +1,33 @@
-export const createAnecdote = (anecdote) => {
-  return {
-    type: "CREATE",
-    data: anecdote
-  }
-}
+import { createSlice } from "@reduxjs/toolkit"
 
-export const voteAnecdote = (id) => {
-  return {
-    type: "VOTE",
-    data: {
-      id
-    }
-  }
-}
-export const setAnecdotes = (data) =>{
-  return {
-    type:"SET",data
-  }
-}
-
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case "VOTE":
-      const id = action.data.id
+const anecdoteSlice = createSlice({
+  name: 'anecdote',
+  initialState: [],
+  reducers: {
+    createAnecdote(state, action) {
+      state.push(action.payload)
+    },
+    voteAnecdote(state, action) {
+      const id = action.payload
       const anecdoteToChange = state.find(n => n.id === id)
       const updatedAnecdote = {
         ...anecdoteToChange,
         votes: anecdoteToChange.votes + 1
       }
-      return state.map(a =>
-        a.id === id ?
-          updatedAnecdote :
-          a
-      )
+      return state
+        .map(a =>
+          a.id === id ?
+            updatedAnecdote :
+            a
+        )
         .sort(
           (a, b) => b.votes - a.votes
         )
-    case "CREATE":
-      return [...state, action.data]
-    case "SET":
-      return action.data
-    default:
-      return state
+    },
+    setAnecdotes(state,action){
+      return action.payload
+    }
   }
-}
-
-export default reducer
+})
+export const {createAnecdote,setAnecdotes,voteAnecdote} = anecdoteSlice.actions
+export default anecdoteSlice.reducer
