de307cfacba5457716a1fd049779c675372f2e66
emmettxt <emmet.txt@gmail.com>
2022-09-23 12:24:44 +0200

    Added like button to blog page

--- part7/blogApp/frontend/src/components/BlogPage.js
+++ part7/blogApp/frontend/src/components/BlogPage.js
@@ -1,12 +1,19 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useParams, useNavigate } from 'react-router-dom'
+import { updateBlog } from '../reducers/blogReducer'
 
 const BlogPage = () => {
   const params = useParams()
   const navigate = useNavigate()
+  const dispatch = useDispatch()
   const blogs = useSelector(state => state.blogs)
   const id = params.id
-
+  const handleLike = async blog => {
+    const newBlog = {
+      likes: blog.likes + 1,
+    }
+    await dispatch(updateBlog(blog.id, newBlog))
+  }
   if (!blogs) {
     navigate('/')
   } else {
@@ -18,7 +25,16 @@ const BlogPage = () => {
         <div>
           <h1>{thisBlog.title}</h1>
           <a href={thisBlog.url}>{thisBlog.url}</a>
-          <div>{thisBlog.likes} likes</div>
+          <div>
+            {thisBlog.likes} likes{' '}
+            <button
+              onClick={() => {
+                handleLike(thisBlog)
+              }}
+            >
+              like
+            </button>
+          </div>
           <div>added by {thisBlog.user ? thisBlog.user.name : null}</div>
         </div>
       )
